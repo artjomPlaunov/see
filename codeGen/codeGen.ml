72e3ecec67b8d1d9db2d35109706d30f5d582265
Artjom Plaunov <artyemnyc@gmail.com>
2021-10-08 16:06:51 -0400

    Handle function calls

--- codeGen/codeGen.ml
+++ codeGen/codeGen.ml
@@ -42,6 +42,12 @@ let getParamReg num =
   | 6 ->  "r9d"
   | _ ->  "rError" 
 
+let getOpInstr op = 
+  match op with
+  | Mul -> "imull"
+  | Add -> "addl"
+  | Sub -> "sub;"
+
 let pushParams res paramLst nextOffset offsetMap = 
   let pLst = match paramLst with ParamLst lst -> lst in
   let rec aux res pLst nextOffset offsetMap paramNum =  
@@ -58,7 +64,7 @@ let rec pushDecls res decls offset offsetMap =
   match decls with
   | []  ->  (res,offset,offsetMap)
   | (Decl (Ident id,value))::t ->
-      let offsetMap = Map.add_exn offsetMap ~key:id ~data:value in
+      let offsetMap = Map.add_exn offsetMap ~key:id ~data:offset in
       let s = Format.asprintf "  movl $%d %d(%%rbp)" value offset in
       pushDecls (res@[s]) t (offset-4) offsetMap
 
@@ -77,10 +83,81 @@ let rec pushAllDecls res allDecls offset offsetMap =
       let (res,offset,offsetMap) =  pushDecls res decls offset offsetMap in
       pushAllDecls res t offset offsetMap
   | (Stm_arrayDecl (Ident id, size, values))::t ->
-      let offsetMap = Map.add_exn offsetMap ~key:id ~data:(offset-(4*size)) in
+      let offsetMap = Map.add_exn offsetMap ~key:id ~data:(offset-(4*(size-1)))
+      in
       let (res,offset) = pushArrayVals res values offset in
       pushAllDecls res t offset offsetMap
   | _ -> (res,offset,offsetMap)
+
+let getRBPOffset id offsetMap = 
+  let offset = 
+    match Map.find offsetMap id with
+    | None -> (-99999)
+    | Some n -> n in
+  Format.asprintf "%d(%%rbp)" offset
+
+let getRBPArrayOffset id idx offsetMap = 
+  let offset = 
+    match Map.find offsetMap id with
+    | None -> (-99999)
+    | Some n -> n in
+  Format.asprintf "%d(%%rbp)" (offset + (idx*4))
+  
+
+let genFunCall res offsetMap eLst =
+  let rec aux res offsetMap eLst argNum = 
+    if argNum > 6 then res else
+    let r = getParamReg argNum in
+    match eLst with
+    | []  ->  res
+    | (Constant v)::t -> 
+        let i1 = Format.asprintf "  movl $%d %s" v r in
+        let res = res@[i1] in
+        aux res offsetMap t (argNum+1)
+    | (Variable (Ident id))::t ->
+        let src = getRBPOffset id offsetMap in
+        let i1 = Format.asprintf "  movl %s %s" src r in
+        aux (res@[i1]) offsetMap t (argNum+1)
+    | (ArraySubscript ((Ident id), idx))::t ->
+        let src = getRBPArrayOffset id idx offsetMap in
+        let i1 = Format.asprintf "  movl %s %s" src r in
+        aux (res@[i1]) offsetMap t (argNum+1)
+    | _ -> res
+  in 
+  aux res offsetMap eLst 1
+  
+
+let genBlock res stms offset offsetMap =
+  let aux res exp _ offsetMap = 
+  match exp with
+    (* Very specific case where destination and sources are all ID's. *)
+    | Assign 
+      (Ident id, Arith (Variable (Ident id1) ,op, Variable (Ident id2))) ->
+        let opInstr = getOpInstr op in
+        let source1 = getRBPOffset id1 offsetMap in
+        let source2 = getRBPOffset id2 offsetMap in
+        let dest    = getRBPOffset id  offsetMap in
+        let i1 = Format.asprintf "  movl %s, %%eax" source1 in
+        let i2 = Format.asprintf "  %s %s, %%eax" opInstr source2 in
+        let i3 = Format.asprintf "  movl %%eax, %s" dest in
+        res@[i1;i2;i3]
+    | FunctionCall ((Ident id), eLst) ->
+        let s = Format.asprintf "  call %s" id in
+        let res = genFunCall res offsetMap eLst in
+        res@[s]
+    | _ -> res
+  in 
+
+  let rec aux' res stms offset offsetMap = 
+  match stms with
+    | []  -> res
+    | (Stm_exp e)::t -> 
+        let res = aux res e offset offsetMap in
+        aux' res t offset offsetMap
+    | _ ->  res
+  in 
+  aux' res stms offset offsetMap
+
 
 let genFun f = 
   let Fun (_,paramLst,Ident id,block) = f in
@@ -92,7 +169,7 @@ let genFun f =
   let res = pushStack res len in
   let (res,offset,offsetMap) = pushParams res paramLst offset offsetMap in 
   let (res,offset,offsetMap) = pushAllDecls res decls offset offsetMap in
-  let res = genBlock res block 
+  let res = genBlock res block offset offsetMap in
   res
 
 (* genProg is the workhorse.
--- codeGen_tests/output.s
+++ codeGen_tests/output.s
@@ -5,21 +5,26 @@ test:
   movl %edi, -4(%rbp)
   movl %esi, -8(%rbp)
   movl $0 -12(%rbp)
+  movl -4(%rbp), %eax
+  sub; -8(%rbp), %eax
+  movl %eax, -12(%rbp)
+  movl -4(%rbp), %eax
+  addl -8(%rbp), %eax
+  movl %eax, -12(%rbp)
+  movl -4(%rbp), %eax
+  imull -8(%rbp), %eax
+  movl %eax, -12(%rbp)
 
 main:
   pushq %rbp
   movq %rsp, %rbp
-  subq $48, %rsp
+  subq $20, %rsp
   movl $1 -20(%rbp)
   movl $2 -16(%rbp)
   movl $3 -12(%rbp)
   movl $4 -8(%rbp)
   movl $5 -4(%rbp)
-  movl $4234 -48(%rbp)
-  movl $42342 -44(%rbp)
-  movl $423423 -40(%rbp)
-  movl $423423 -36(%rbp)
-  movl $423423 -32(%rbp)
-  movl $423423 -28(%rbp)
-  movl $42343 -24(%rbp)
+  movl -20(%rbp) edi
+  movl $100 esi
+  call test
 
